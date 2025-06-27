ac007d05e1e0e6e31b4aeff1680f741316ad5ddc
Caleb LeNoir <1080797+calebl@users.noreply.github.com>
2025-06-27 15:58:42 -0400

    update dockerfile and remove mailerlite

--- app/models/concerns/mailing_list.rb
+++ app/models/concerns/mailing_list.rb
@@ -2,7 +2,7 @@ module MailingList
   MAILER_LITE_GROUP = 111712173989824242
 
   def subscribe_to_mailing
-    MailerLite.create_group_subscriber(MAILER_LITE_GROUP, { email: email }) if Rails.env.production?
+    # MailerLite.create_group_subscriber(MAILER_LITE_GROUP, { email: email }) if Rails.env.production?
   end
 
 end
--- config/initializers/mailerlite.rb
+++
@@ -1,4 +0,0 @@
-MailerLite.configure do |config|
-  config.api_key = Rails.application.credentials.mailerlite[:api_key] if Rails.env.production?
-  # config.timeout = 10
-end
